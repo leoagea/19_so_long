86dd34043ddc7c60c9d0abcec2b134a5ea2e374f
leoagea <leoagea@gmail.com>
2024-05-24 20:16:58 +0200

    Add : add funct exit_message write error\n, then write the error message define in so_long.h then exit

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,21 +6,45 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 17:04:03 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/24 19:01:48 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/24 20:13:19 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
-#include "libft.h"
-#include <mlx.h>
+# include "libft.h"
+# include <mlx.h>
 
-/*====================Parser====================*/
+#define EMPTY "Map file is empty"
+#define NOT_RECT "Map is not rectangular"
 
+typedef struct s_map
+{
+	int		fd;
+	int		x;
+	int		y;
+	char	*path;
+	char	**layout;
+
+}			t_map;
+
+typedef struct s_data
+{
+	t_map	map;
+
+}			t_data;
+
+/*====================PARSER====================*/
 /*---------------------Map----------------------*/
 
-char **open_map(char *path);
-char **read_map(int fd);
+void open_map(t_data *data);
+void get_malloc_size(t_data *data);
+// char		**read_map(int fd);
+
+/*=====================EXIT=====================*/
+/*---------------------exit---------------------*/
+
+void exit_message(char *str);
 
 #endif
