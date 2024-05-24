64ead8d80590f831fc31af3aa93ca7731829aebd
leoagea <leoagea@gmail.com>
2024-05-24 20:22:50 +0200

    Add : funct get_malloc_size count the number of line to malloc, register the len of the first line and exit if the size of all len is not similar

--- src/parser/map.c
+++ src/parser/map.c
@@ -6,24 +6,75 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 17:35:54 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/24 19:05:21 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/24 20:21:46 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/so_long.h"
 
-char **open_map(char *path)
+void open_map(t_data *data)
 {
-	int op;
-	char *read;
 	char **map;
 	
-	op = open(path, O_RDONLY);
-	if (op == -1)
+	data->map.fd = open(data->map.path, O_RDONLY);
+	if (data->map.fd == -1)
 	{
 		ft_printf("Error\n");
 		ft_printf("%s\n",strerror(errno));
 		exit(EXIT_FAILURE);
 	}
+	// read_map(data->map.fd);
+	get_malloc_size(data);
+	close(data->map.fd);
+	open(data->map.path, O_RDONLY);
+	close(data->map.fd);
 }
 
+void get_malloc_size(t_data *data)
+{
+	int len;
+	char *read;
+	
+	read = get_next_line(data->map.fd);
+	if (read == NULL)
+		exit_message(EMPTY);
+	data->map.x = ft_strlen(read) - 1;
+	while(read != NULL)
+	{
+		data->map.y += 1;
+		free(read);
+		read = get_next_line(data->map.fd);
+		len = ft_strlen(read) - 1;
+		if (read == NULL)
+			break ;
+		if (data->map.x != len )
+		{
+			free(read);
+			exit_message(NOT_RECT);
+		}
+	}
+	free(read);
+	printf("nbr ligne : %d, long ligne: %d\n",data->map.y, data->map.x);
+}
+
+void parse_map(t_data)
+// char **read_map(int fd)
+// {
+// 	char *read;
+// 	char **map;
+	
+// 	read = get_next_line(fd);
+// 	if (read == NULL)
+// 		exit(EXIT_FAILURE);
+// 	while (read != NULL)
+// 	{
+// 		printf("%s\n",read);
+// 		// map[i] = read;
+// 		free(read);
+// 		read = get_next_line(fd);
+// 	// 	i++;
+// 	}
+// 	// map[i] = NULL;
+// 	// free(read);
+// 	return (map);
+// }
