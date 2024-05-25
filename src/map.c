808728f456b172ddf6bfd48f73c0a8ef234a51f0
leoagea <leoagea@gmail.com>
2024-05-25 17:50:31 +0200

    Need : segfault segfault in get_info_map

--- src/map.c
+++ src/map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 17:35:54 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/24 23:16:46 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/25 17:50:10 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,6 @@ void open_map(t_data *data)
 	}
 	// read_map(data->map.fd);
 	get_malloc_size(data);
-	close(data->map.fd);
-	data->map.layout = malloc(sizeof(char *) * (data->map.y + 1));
-	open(data->map.path, O_RDONLY);
 	parse_map(data);
 	close(data->map.fd);
 }
@@ -38,6 +35,9 @@ void get_malloc_size(t_data *data)
 	read = get_next_line(data->map.fd);
 	if (read == NULL)
 		exit_message(EMPTY);
+	else if (ft_strlen(read) == 1)
+		exit_message(INV);
+	printf("%s",read);
 	data->map.x = ft_strlen(read) - 1;
 	while(read != NULL)
 	{
@@ -45,39 +45,77 @@ void get_malloc_size(t_data *data)
 		free(read);
 		read = get_next_line(data->map.fd);
 		len = ft_strlen(read) - 1;
-		if (read == NULL)
+		if (read == NULL || read[1] == '\n')
 			break ;
-		if (data->map.x != len )
-		{
-			free(read);
-			exit_message(NOT_RECT);
-		}
+		printf("%s",read);
 	}
 	free(read);
+	close(data->map.fd);
 	printf("nbr ligne : %d, long ligne: %d\n",data->map.y, data->map.x);
 }
+// add funcrtion to check if last line is strlen - 1 de line 
 
 void parse_map(t_data *data)
 {
-	(void) data;
-}
-// char **read_map(int fd)
-// {
-// 	char *read;
-// 	char **map;
+	int i;
+	// char *read;
 	
-// 	read = get_next_line(fd);
-// 	if (read == NULL)
-// 		exit(EXIT_FAILURE);
-// 	while (read != NULL)
-// 	{
-// 		printf("%s\n",read);
-// 		// map[i] = read;
-// 		free(read);
-// 		read = get_next_line(fd);
-// 	// 	i++;
-// 	}
-// 	// map[i] = NULL;
-// 	// free(read);
-// 	return (map);
-// }
+	i = 0;
+	data->map.layout = (char **) malloc((data->map.y + 1) * sizeof(char *));
+	open(data->map.path, O_RDONLY);
+	while (i < data->map.y)
+	{
+		data->map.layout[i] = get_next_line(data->map.fd);
+		get_info_map(i, data);
+		printf("%s",data->map.layout[i]);
+		i++;
+	}
+	data->map.layout[i] = NULL;
+	printf("Apres parsing : \n");
+	i = 0;
+	while (i < data->map.y)
+	{
+		printf("%s",data->map.layout[i]);
+		i++;
+	}
+	// printf("Player, count : %d, x : %d, y : %d\n",data->player.count, data->player.x, data->player.y);
+	// printf("Exit, count : %d, x : %d, y : %d\n",data->exit.count, data->exit.x, data->exit.y);
+	// printf("Nbr collec : %d\n",data->collec.count);
+	// printf("Nbr ennemy : %d\n",data->ennemy.count);
+}
+
+void get_info_map(int i, t_data *data)
+{
+	// if (i != data->map.y)
+	// 	if ((int)ft_strlen(data->map.layout[i]) != data->map.x)
+	// 		exit_message(NOT_RECT);
+	// if (i == data->map.y)
+	// 	if ((int)ft_strlen(data->map.layout[i]) != (data->map.x - 1))
+	// 		exit_message(NOT_RECT);
+	int j;
+
+	j = 0;
+	while (data->map.layout[i])
+	{
+		if (data->map.layout[i][j] == 'C')
+			data->collec.count++;
+		else if (data->map.layout[i][j] == 'P')
+		{
+			data->player.x = j;
+			data->player.y = i;
+			data->player.count++;
+		}
+		else if (data->map.layout[i][j] == 'E')
+		{	
+			data->exit.x = j;
+			data->exit.y = i;
+			data->exit.count++;
+		}
+		else if (data->map.layout[i][j] == 'X')
+			data->ennemy.count++;
+		j++;
+	}	
+
+	// (void)data;
+	// (void)i;
+}
