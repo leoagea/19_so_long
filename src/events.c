686db9966b3615b5cfb2cf1d0084cd4c7a342ad7
leoagea <leoagea@gmail.com>
2024-05-28 18:00:29 +0200

    Update : next move wall if next move is a wall do nothig

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 17:46:38 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/28 14:31:28 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/28 17:59:36 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,28 @@ int when_destroy(t_data *data)
 int when_keypress(int keysym, t_data *data)
 {
 	data->count++;
-	render_xpm(data, data->xpm.ground, data->player.x * PIXEL, data->player.y * PIXEL);
+	render_xpm(data, data->xpm.ground, data->player.x * PIXEL, (data->player.y + 2) * PIXEL);
 	if (keysym == ESC)
 		when_destroy(data);
 	else if (keysym == W || keysym == AR_U)
 	{
 		if (move(data, (data->player.x), (data->player.y - 1)) == 0)
-			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, data->player.y * PIXEL);
+			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, (data->player.y + 2) * PIXEL);
 	}
 	else if (keysym == D || keysym == AR_R)
 	{
 		if (move(data, (data->player.x + 1), (data->player.y)) == 0)
-			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, data->player.y * PIXEL);
+			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, (data->player.y + 2) * PIXEL);
 	}
 	else if (keysym == A || keysym == AR_L)
 	{
 		if (move(data, (data->player.x - 1), (data->player.y)) == 0)
-			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, data->player.y * PIXEL);
+			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, (data->player.y + 2) * PIXEL);
 	}
 	else if (keysym == S || keysym == AR_D)
-	{
 		if (move(data, (data->player.x), (data->player.y + 1)) == 0)
-			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, data->player.y * PIXEL);
-	}
-	printf("Moves : %d	Key pressed : %d\n", data->count, keysym);
+			render_xpm(data, data->xpm.player, (data->player.x) * PIXEL, (data->player.y + 2) * PIXEL);
+	place_background_text(data);
 	return (1);
 }
 
@@ -54,7 +52,9 @@ int move(t_data *data, int x, int y)
 {
 	if (data->map.layout[y][x] == '1')
 		return 0;
-	if (data->map.layout[y][x] == 'E')
+	if (data->map.layout[y][x] == 'E' && data->collec.game != data->collec.count)
+		return 0;
+	if (data->map.layout[y][x] == 'E' && data->collec.game == data->collec.count)
 		when_destroy(data);
 	if (data->map.layout[y][x] == 'X')
 		when_destroy(data);
@@ -63,12 +63,12 @@ int move(t_data *data, int x, int y)
 		data->collec.game++;
 		data->map.layout[y][x] = '0';
 	}
-	render_xpm(data, data->xpm.ground, x * PIXEL, y * PIXEL);
-	render_xpm(data, data->xpm.player, x * PIXEL, y * PIXEL);
+	render_xpm(data, data->xpm.ground, x * PIXEL, (y + 2) * PIXEL);
+	render_xpm(data, data->xpm.player, x * PIXEL, (y + 2) * PIXEL);
 	if (data->collec.game == data->collec.count)
 	{
-		render_xpm(data, data->xpm.ground, data->exit.x * PIXEL, data->exit.y * PIXEL);
-		render_xpm(data, data->xpm.ladder, data->exit.x * PIXEL, data->exit.y * PIXEL);
+		render_xpm(data, data->xpm.ground, data->exit.x * PIXEL, (data->exit.y + 2)* PIXEL);
+		render_xpm(data, data->xpm.ladder, data->exit.x * PIXEL, (data->exit.y + 2) * PIXEL);
 	}
 	data->player.x = x;
 	data->player.y = y;
