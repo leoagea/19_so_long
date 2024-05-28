fd90dfc4bb3a560160e2939d2c1d4eb801ff7ae8
leoagea <leoagea@gmail.com>
2024-05-28 18:32:55 +0200

    Add : add funct place background text

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 23:32:36 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/28 14:24:59 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/28 18:30:03 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ void render_map(t_data *data)
     data->mlx.mlx = mlx_init();
 	if (!data->mlx.mlx)
 		exit(EXIT_FAILURE);
-	data->mlx.win = mlx_new_window(data->mlx.mlx, (data->map.x) * PIXEL, (data->map.y) * PIXEL, "So_long");
+	data->mlx.win = mlx_new_window(data->mlx.mlx, (data->map.x) * PIXEL, (data->map.y + 2) * PIXEL, "So_long");
 	if (!data->mlx.win)
 	{
 		free(data->mlx.mlx);
 		exit(EXIT_FAILURE);
 	}
-
+	mlx_do_sync(data->mlx.mlx);
 	mlx_hook(data->mlx.win,KeyPress,KeyPressMask,&when_keypress,data);
 	mlx_hook(data->mlx.win, DestroyNotify, 0, &when_destroy, data);
 	load_xpm(data);
@@ -64,9 +64,7 @@ void place_map(t_data *data)
 {
 	int x;
 	int y;
-	// int count;
 
-	// count = 0;
 	y = 0;
 	printf("\nx : %d, y : %d",data->map.x, data->map.y);
 	while (y < (data->map.y ))
@@ -82,6 +80,7 @@ void place_map(t_data *data)
 		}
 		y++;
 	}
+	place_background_text(data);
 	printf("\nplayer x : %d, y : %d\n", data->player.x, data->player.y);
 	printf("collectable : %d , exit : %d , player : %d\n", data->collec.count, data->exit.count, data->player.count);
 }
@@ -89,32 +88,45 @@ void place_map(t_data *data)
 void place_items(t_data *data, int x, int y)
 {
 	if (data->map.layout && data->map.layout[y ][x ] == '1')
-	{
-		render_xpm(data, data->xpm.wall, x * PIXEL, y * PIXEL);
-	}
+		render_xpm(data, data->xpm.wall, x * PIXEL, (y + 2) * PIXEL);
 	else if (data->map.layout && data->map.layout[y ][x ] == '0')
-	{
-		render_xpm(data, data->xpm.ground, x* PIXEL, y* PIXEL);
-	}
+		render_xpm(data, data->xpm.ground, x * PIXEL, (y + 2) * PIXEL);
 	else if (data->map.layout && data->map.layout[y ][x ] == 'C')
-	{
-		// render_xpm(data, data->xpm.ground, x* PIXEL, y* PIXEL);
-		render_xpm(data, data->xpm.coin, x* PIXEL, y* PIXEL);
-	}
+		render_xpm(data, data->xpm.coin, x* PIXEL, (y + 2) * PIXEL);
 	else if (data->map.layout && data->map.layout[y ][x ] == 'P')
 	{
-		render_xpm(data, data->xpm.ground, x* PIXEL, y* PIXEL);
-		render_xpm(data, data->xpm.player, x* PIXEL, y* PIXEL);
+		render_xpm(data, data->xpm.ground, x* PIXEL, (y + 2) * PIXEL);
+		render_xpm(data, data->xpm.player, x* PIXEL, (y + 2) * PIXEL);
 	}
 	else if (data->map.layout && data->map.layout[y ][x ] == 'E')
-	{
-		render_xpm(data, data->xpm.trapdoor, x* PIXEL, y* PIXEL);
-	}
+		render_xpm(data, data->xpm.trapdoor, x* PIXEL, (y + 2) * PIXEL);
 	else if (data->map.layout && data->map.layout[y ][x ] == 'X')
 	{
-		render_xpm(data, data->xpm.ground, x* PIXEL, y* PIXEL);
-		render_xpm(data, data->xpm.ennemy, x* PIXEL, y* PIXEL);
+		render_xpm(data, data->xpm.ground, x * PIXEL, (y + 2) * PIXEL);
+		render_xpm(data, data->xpm.ennemy, x * PIXEL, (y + 2) * PIXEL);
 	}
-	else
-		return ;
+}
+
+void place_background_text(t_data *data)
+{
+	int x;
+	int y;
+	char *count;
+	
+	y = 0;
+	while (y < 2)
+	{
+		x = 0;
+		while (x < data->map.x)
+		{
+			render_xpm(data, data->xpm.ground, x * PIXEL, y * PIXEL);
+			x++;	
+		}
+		y++;
+	}
+	count = ft_itoa(data->count);
+	mlx_string_put(data->mlx.mlx, data->mlx.win, 0, 0, 12895487, "Moves :");
+	mlx_string_put(data->mlx.mlx, data->mlx.win, 80, 0, 12895487, count);
+	free(count);
+	ft_printf("Moves : %d\n", data->count);
 }
