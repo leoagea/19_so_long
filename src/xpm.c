1529ba96bcea0970133e6a950a801cbc5086132f
leoagea <leoagea@gmail.com>
2024-06-04 17:25:38 +0200

    Fix : load_xpm call wrong repo for assets replace assets_24x24 by assets

--- src/xpm.c
+++ src/xpm.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 17:12:16 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/31 17:15:35 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/04 17:24:04 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ int	load_xpm(t_data *data)
 	int	y;
 
 	data->xpm.wall = mlx_xpm_file_to_image(data->mlx.mlx,
-			"assets_24x24/wall.xpm", &x, &y);
+			"assets/wall.xpm", &x, &y);
 	if (!data->xpm.wall)
 		return (-1);
 	data->xpm.coin = mlx_xpm_file_to_image(data->mlx.mlx,
-			"assets_24x24/coin.xpm", &x, &y);
+			"assets/coin.xpm", &x, &y);
 	if (!data->xpm.wall)
 		return (-1);
 	data->xpm.player = mlx_xpm_file_to_image(data->mlx.mlx,
-			"assets_24x24/player.xpm", &x, &y);
+			"assets/player.xpm", &x, &y);
 	if (!data->xpm.wall)
 		return (-1);
 	data->xpm.ennemy = mlx_xpm_file_to_image(data->mlx.mlx,
-			"assets_24x24/ennemy.xpm", &x, &y);
+			"assets/ennemy.xpm", &x, &y);
 	if (!data->xpm.wall)
 		return (-1);
 	if (load_xpm_2(data, x, y) == -1)
@@ -41,15 +41,15 @@ int	load_xpm(t_data *data)
 int	load_xpm_2(t_data *data, int x, int y)
 {
 	data->xpm.ground = mlx_xpm_file_to_image(data->mlx.mlx,
-			"assets_24x24/ground.xpm", &x, &y);
+			"assets/ground.xpm", &x, &y);
 	if (!data->xpm.wall)
 		return (-1);
 	data->xpm.ladder = mlx_xpm_file_to_image(data->mlx.mlx,
-			"assets_24x24/ladder.xpm", &x, &y);
+			"assets/ladder.xpm", &x, &y);
 	if (!data->xpm.wall)
 		return (-1);
 	data->xpm.trapdoor = mlx_xpm_file_to_image(data->mlx.mlx,
-			"assets_24x24/trapdoor.xpm", &x, &y);
+			"assets/trapdoor.xpm", &x, &y);
 	if (!data->xpm.wall)
 		return (-1);
 	return (1);
